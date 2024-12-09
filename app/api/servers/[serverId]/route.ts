e1cb2455a1738711c9a835ed8678d38600e7b914
ExtremeDie <poringkyz05059@gmail.com>
2024-12-09 15:12:11 +0800

    complete edit server function

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+import currentProfile from '../../../../lib/current-profile';
+import { db } from '../../../../lib/db';
+
+export async function PATCH(req: Request, { params }: { params: { serverId: string } }) {
+	try {
+		const { serverId } = await params;
+		const profile = await currentProfile();
+		const { name, imageUrl } = await req.json();
+
+		if (!profile) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+
+		if (!serverId) {
+			return new NextResponse('Server ID missing', { status: 400 });
+		}
+
+		const server = await db.server.update({
+			where: {
+				id: serverId,
+				profileId: profile.id,
+			},
+			data: {
+				name,
+				imageUrl,
+			},
+		});
+
+		return NextResponse.json(server);
+	} catch (err) {
+		console.log('SERVER_ID_PATCH', err);
+		return new NextResponse('Internal Error', { status: 500 });
+	}
+}
